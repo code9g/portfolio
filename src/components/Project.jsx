dd7668b78671faa22116da161e62ecf14c14d6f5
Pierre HENRY <pierre.henry@code99.fr>
2024-12-24 11:15:00 +0100

    Gestion de l'url de base pour les imgs dans public

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,14 +5,14 @@ import Tags from "./Tags";
 const Project = ({ images, title, description, skills }) => {
   const firstPicture =
     images instanceof Array && images.length > 0 ? images[0] : null;
-
+  console.log(import.meta.env.BASE_URL);
   return (
     <article className="flex flex-wrap gap-8 lg:justify-center">
       <div className="w-full lg:w-1/4">
         {firstPicture ? (
           <img
             className="aspect-square w-full rounded object-contain object-top"
-            src={`portfolio/img/projects/${firstPicture}`}
+            src={`${import.meta.env.BASE_URL}/img/projects/${firstPicture}`}
             alt={title}
           />
         ) : (
