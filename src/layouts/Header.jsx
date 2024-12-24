4a5866306f35f3cc4ee6d016b441d58bc5ea2524
Pierre HENRY <pierre.henry@code99.fr>
2024-12-24 11:56:00 +0100

    Ajout menu burger

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,3 +1,6 @@
+import clsx from "clsx";
+import { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
 import Container from "../components/Container";
 
 const links = [
@@ -10,15 +13,35 @@ const links = [
 ];
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="bg-neutral-950">
       <Container className="flex items-center justify-between text-xl">
-        <a href="#home">&lt;code99&nbsp;/&gt;</a>
-        <nav className="flex items-center justify-between">
-          <ul className="flex">
+        <a className="pl-4 lg:pl-0" href="#home">
+          &lt;code99&nbsp;/&gt;
+        </a>
+        <nav className="relative flex items-center justify-between">
+          <button
+            className="pr-4 lg:hidden lg:pr-0"
+            type="button"
+            onClick={() => setIsOpen((curr) => !curr)}
+          >
+            <CiMenuBurger />
+          </button>
+          <ul
+            className={clsx(
+              "absolute right-0 top-[125%] rounded-md bg-neutral-950 p-5 lg:relative lg:flex lg:rounded-none lg:bg-transparent lg:p-0",
+              !isOpen && "hidden"
+            )}
+          >
             {links.map((link, index) => (
               <li key={index}>
-                <a className="px-3 hover:underline" href={link.url}>
+                <a
+                  className="px-3 hover:underline"
+                  href={link.url}
+                  onClick={() => setIsOpen(false)}
+                >
                   {link.title}
                 </a>
               </li>
