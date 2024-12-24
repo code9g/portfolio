c86836d8ab23d18f31f790ab3909d2e354cdfb3f
Pierre HENRY <pierre.henry@code99.fr>
2024-12-24 15:42:11 +0100

    Chgment border width

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -17,7 +17,7 @@ const Skill = ({ name }) => {
   );
 
   return (
-    <div className="group relative flex size-28 items-center justify-center rounded-2xl border-4 border-neutral-500 p-4">
+    <div className="group relative flex size-28 items-center justify-center rounded-2xl border-2 border-neutral-500 p-4">
       {content}
       <div className="absolute bottom-0 z-10 hidden translate-y-[125%] rounded-md border border-neutral-400 bg-neutral-950/50 px-4 py-1 group-hover:block">
         {icon.title}
