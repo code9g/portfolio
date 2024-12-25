8079aab06d950c08129370e4ead3aeed19391005
Pierre HENRY <pierre.henry@code99.fr>
2024-12-26 00:12:18 +0100

    Design page NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="flex h-full max-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl text-red-400">404</h1>
+      <p className="text-xl text-neutral-300">
+        Oups ! Nous n&apos;avons pas pu trouver cette ressource !
+      </p>
+      <Link className="" to="/">
+        Acceuil
+      </Link>
+    </div>
+  );
 };
 
 NotFound.propTypes = {};
