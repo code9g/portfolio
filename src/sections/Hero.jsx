2c076f273a435b2ad6ac29c973abc83092a67753
Pierre HENRY <pierre.henry@code99.fr>
2024-12-26 02:40:47 +0100

    retrait div

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,32 +7,28 @@ const Hero = () => {
   return (
     <Section id="hero" className="flex flex-wrap">
       <motion.div
-        className="w-full lg:w-1/2"
+        className="flex w-full flex-col items-center lg:w-1/2 lg:items-start"
         initial={{ x: -100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        <div className="flex flex-col items-center lg:items-start">
-          <h2 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
-            {resume.fullName}
-          </h2>
-          <h3 className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
-            {resume.role}
-          </h3>
-          <p className="my-2 max-w-xl py-6 text-justify font-light tracking-tighter">
-            {resume.hero}
-          </p>
-        </div>
+        <h2 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+          {resume.fullName}
+        </h2>
+        <h3 className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+          {resume.role}
+        </h3>
+        <p className="my-2 max-w-xl py-6 text-justify font-light tracking-tighter">
+          {resume.hero}
+        </p>
       </motion.div>
       <motion.div
-        className="w-full lg:w-1/2 lg:p-8"
+        className="flex w-full justify-center lg:w-1/2 lg:p-8"
         initial={{ x: +100, opacity: 0 }}
         whileInView={{ x: 0, opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        <div className="flex justify-center">
-          <img src={profile} alt={resume.fullName} />
-        </div>
+        <img src={profile} alt={resume.fullName} />
       </motion.div>
     </Section>
   );
