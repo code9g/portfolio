1990c72fcdc69a8cc6e8ec9a534e45c963a4ac5f
Pierre HENRY <pierre.henry@code99.fr>
2024-12-28 02:24:04 +0100

    Amélioration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,20 @@
+import clsx from "clsx";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import findIcons from "../lib/utils/findIcons";
 
-const Contact = ({ type, url, label }) => {
+const Contact = ({ type, url, label, className }) => {
   const icon = findIcons(type);
 
   return (
     <>
-      {icon && <icon.component alt={icon.title} />}
-      <Link className="hover:underline" to={url}>
+      {icon && (
+        <icon.component
+          alt={icon.title}
+          className={clsx(icon.className, className)}
+        />
+      )}
+      <Link className={clsx("hover:underline", className)} to={url}>
         {label}
       </Link>
     </>
@@ -19,6 +25,7 @@ Contact.propTypes = {
   type: PropTypes.string,
   url: PropTypes.string,
   label: PropTypes.string,
+  className: PropTypes.any,
 };
 
 export default Contact;
