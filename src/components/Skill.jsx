16e001d23101eabb65a799639e761c2ada47227c
Pierre HENRY <pierre.henry@code99.fr>
2025-01-05 13:00:46 +0100

    Chmg tooltip

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -4,6 +4,7 @@ import findIcons from "../lib/utils/findIcons";
 
 const Skill = ({ name }) => {
   const icon = findIcons(name);
+
   const content = icon ? (
     <icon.component
       className={clsx("size-full", icon.className)}
@@ -16,7 +17,7 @@ const Skill = ({ name }) => {
   return (
     <div className="group relative flex size-28 items-center justify-center rounded-2xl border-2 border-neutral-500 p-4">
       {content}
-      <div className="absolute bottom-0 z-10 hidden translate-y-[125%] rounded-md border border-neutral-400 bg-neutral-950/50 px-4 py-1 group-hover:block">
+      <div className="absolute left-1/2 top-full z-10 mb-2 hidden -translate-x-1/2 select-none text-nowrap  rounded-md border border-neutral-400 bg-neutral-950/70 px-4 py-1 group-hover:block">
         {icon.title}
       </div>
     </div>
